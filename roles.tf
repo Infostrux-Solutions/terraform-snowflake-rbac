04668c1312c81c09b2153e9e10ebbd606a37aebb
Craig Sloggett <craig@infostrux.com>
2022-09-20 11:05:34 -0400

    Reverse role grants and add Snowflake grants

--- roles.tf
+++ roles.tf
@@ -9,6 +9,15 @@ resource "snowflake_role_grants" "role_grants" {
   for_each = {
     for pair in local.role_grants_map : "${pair.role}.${pair.grant}" => pair
   }
-  role_name = snowflake_role.roles[each.value.role].name
-  roles     = [snowflake_role.roles[each.value.grant].name]
+  role_name = snowflake_role.roles[each.value.grant].name
+  roles     = [snowflake_role.roles[each.value.role].name]
+}
+
+resource "snowflake_role_grants" "snowflake_role_grants" {
+  provider = snowflake.securityadmin
+  for_each = {
+    for pair in local.snowflake_role_grants_map : "${pair.role}.${pair.grant}" => pair
+  }
+  role_name = snowflake_role.roles[each.value.grant].name
+  roles     = [each.value.role]
 }
