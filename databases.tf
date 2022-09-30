c3e5a6bd6a344048537a919b1d7463746fd42d2f
Craig Sloggett <craig@infostrux.com>
2022-09-30 18:54:58 -0400

    Add enable_multiple_grants to fix roles being deprovisioned

--- databases.tf
+++ databases.tf
@@ -1,61 +1,67 @@
 # Database Privileges
 
 resource "snowflake_database_grant" "database_read_grants" {
-  provider      = snowflake.securityadmin
-  for_each      = local.database_read_grants
-  database_name = each.value.database_name
-  privilege     = each.value.privilege
-  roles         = each.value.roles
+  provider               = snowflake.securityadmin
+  for_each               = local.database_read_grants
+  database_name          = each.value.database_name
+  privilege              = each.value.privilege
+  roles                  = each.value.roles
+  enable_multiple_grants = true
 }
 
 resource "snowflake_database_grant" "database_write_grants" {
-  provider      = snowflake.securityadmin
-  for_each      = local.database_write_grants
-  database_name = each.value.database_name
-  privilege     = each.value.privilege
-  roles         = each.value.roles
+  provider               = snowflake.securityadmin
+  for_each               = local.database_write_grants
+  database_name          = each.value.database_name
+  privilege              = each.value.privilege
+  roles                  = each.value.roles
+  enable_multiple_grants = true
 }
 
 # Schema Privileges
 
 resource "snowflake_schema_grant" "schema_read_grants" {
-  provider          = snowflake.securityadmin
-  for_each          = local.schema_read_grants
-  database_name     = each.value.database_name
-  privilege         = each.value.privilege
-  roles             = each.value.roles
-  on_future         = true
-  with_grant_option = false
+  provider               = snowflake.securityadmin
+  for_each               = local.schema_read_grants
+  database_name          = each.value.database_name
+  privilege              = each.value.privilege
+  roles                  = each.value.roles
+  enable_multiple_grants = true
+  on_future              = true
+  with_grant_option      = false
 }
 
 resource "snowflake_schema_grant" "schema_write_grants" {
-  provider          = snowflake.securityadmin
-  for_each          = local.schema_write_grants
-  database_name     = each.value.database_name
-  privilege         = each.value.privilege
-  roles             = each.value.roles
-  on_future         = true
-  with_grant_option = false
+  provider               = snowflake.securityadmin
+  for_each               = local.schema_write_grants
+  database_name          = each.value.database_name
+  privilege              = each.value.privilege
+  roles                  = each.value.roles
+  enable_multiple_grants = true
+  on_future              = true
+  with_grant_option      = false
 }
 
 # Table Privileges
 
 resource "snowflake_table_grant" "table_read_grants" {
-  provider          = snowflake.securityadmin
-  for_each          = local.table_read_grants
-  database_name     = each.value.database_name
-  privilege         = each.value.privilege
-  roles             = each.value.roles
-  on_future         = true
-  with_grant_option = false
+  provider               = snowflake.securityadmin
+  for_each               = local.table_read_grants
+  database_name          = each.value.database_name
+  privilege              = each.value.privilege
+  roles                  = each.value.roles
+  enable_multiple_grants = true
+  on_future              = true
+  with_grant_option      = false
 }
 
 resource "snowflake_table_grant" "table_write_grants" {
-  provider          = snowflake.securityadmin
-  for_each          = local.table_write_grants
-  database_name     = each.value.database_name
-  privilege         = each.value.privilege
-  roles             = each.value.roles
-  on_future         = true
-  with_grant_option = false
+  provider               = snowflake.securityadmin
+  for_each               = local.table_write_grants
+  database_name          = each.value.database_name
+  privilege              = each.value.privilege
+  roles                  = each.value.roles
+  enable_multiple_grants = true
+  on_future              = true
+  with_grant_option      = false
 }
