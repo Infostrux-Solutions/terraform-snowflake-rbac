c3421c127e4546b84d8e699af7e7b1039aefc208
Craig Sloggett <craig@infostrux.com>
2022-10-03 14:59:48 -0400

    feat: Convert to Terraform Module (#5)
    - Updating the `README` documentation to provide usage instructions as a
    Terraform module.
    - Finalizing the `spec.yml` layout to account for future modules using
    the same `spec.yml` file.
    - Add `enable_multiple_grants` parameter to resources to allow for
    resource creation outside of Terraform.
    - Refactoring `spec.yml` structure to accommodate how Terraform expects
    resource definitions (simplifying `local.tf`).

--- variables.tf
+++ variables.tf
@@ -1,27 +1,4 @@
-# Provider
-variable "snowflake_account" {
+variable "spec_file_path" {
   type        = string
-  description = "The Snowflake account that we will be deploying into."
-}
-
-variable "snowflake_username" {
-  type        = string
-  description = "The name of the Snowflake user used to deploy into the Snowflake account."
-}
-
-variable "snowflake_private_key_path" {
-  type        = string
-  description = "The path to the private key used to authenticate the Snowflake user."
-  sensitive   = true
-}
-
-variable "snowflake_private_key_passphrase" {
-  type        = string
-  description = "The passphrase for the private key used to authenticate the Snowflake user."
-  sensitive   = true
-}
-
-variable "snowflake_role" {
-  type        = string
-  description = "The Snowflake role used to deploy into the Snowflake account."
+  description = "The path to the RBAC specification file."
 }
