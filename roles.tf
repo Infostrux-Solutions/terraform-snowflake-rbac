d74a53d049d53e58156d382b1f872330098397be
Craig Sloggett <craig@infostrux.com>
2022-10-03 14:59:48 -0400

    feat: Convert to Terraform Module (#5)
    - Updating the `README` documentation to provide usage instructions as a
    Terraform module.
    - Finalizing the `spec.yml` layout to account for future modules using
    the same `spec.yml` file.
    - Add `enable_multiple_grants` parameter to resources to allow for
    resource creation outside of Terraform.
    - Refactoring `spec.yml` structure to accommodate how Terraform expects
    resource definitions (simplifying `local.tf`).

--- roles.tf
+++ roles.tf
@@ -1,12 +1,10 @@
 resource "snowflake_role_grants" "role_grants" {
-  provider  = snowflake.securityadmin
   for_each  = local.role_grants
   role_name = each.value.role_name
   roles     = each.value.roles
 }
 
 resource "snowflake_role_grants" "user_grants" {
-  provider  = snowflake.securityadmin
   for_each  = local.user_grants
   role_name = each.value.role_name
   users     = each.value.users
---
+++ versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = ">= 0.46.0"
+    }
+  }
+}
